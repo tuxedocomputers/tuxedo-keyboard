1beae3c5a92cd9e2b922a1f8b456a5a87f1d731a
Christoffer Sandberg <cs@tuxedo.de>
2020-12-09 13:17:20 +0100

    Update license headers with outdated address

--- src/uw_io.h
+++ src/uw_io.h
@@ -1,22 +1,21 @@
-/*
-* uw_io.h
-*
-* Copyright (C) 2018-2020 TUXEDO Computers GmbH <tux@tuxedocomputers.com>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*/
+/*!
+ * Copyright (c) 2020 TUXEDO Computers GmbH <tux@tuxedocomputers.com>
+ *
+ * This file is part of tuxedo-keyboard.
+ *
+ * tuxedo-keyboard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef UW_IO_H
 #define UW_IO_H
 
