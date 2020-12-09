912c0ca109568d0730c6e674ce6d261f600eae19
Christoffer Sandberg <cs@tuxedo.de>
2020-12-09 12:42:00 +0100

    Adjust some old references

--- src/tuxedo_io/tongfang_wmi.h
+++ src/tuxedo_io/tongfang_wmi.h
@@ -1,9 +1,9 @@
 /*!
  * Copyright (c) 2020 TUXEDO Computers GmbH <tux@tuxedocomputers.com>
  *
- * This file is part of tuxedo-cc-wmi.
+ * This file is part of tuxedo-io.
  *
- * tuxedo-cc-wmi is free software: you can redistribute it and/or modify
+ * tuxedo-io is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
