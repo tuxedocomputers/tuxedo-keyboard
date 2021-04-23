5ff4174be9de79dd03fe17cd83d4318487019e65
Christoffer Sandberg <cs@tuxedo.de>
2021-04-23 15:05:15 +0200

    Add XP gen 11 & 12 to perf. profile workaround

--- src/clevo_keyboard.h
+++ src/clevo_keyboard.h
@@ -810,6 +810,8 @@ int clevo_keyboard_init(void)
 		|| dmi_string_in(DMI_BOARD_NAME, "NV4XMB,ME,MZ")
 		|| dmi_string_in(DMI_BOARD_NAME, "L140CU")
 		|| dmi_string_in(DMI_BOARD_NAME, "NS50MU")
+		|| dmi_string_in(DMI_BOARD_NAME, "PCX0DX")
+		|| dmi_string_in(DMI_BOARD_NAME, "PCx0Dx_GN20")
 		;
 	if (performance_profile_set_workaround) {
 		TUXEDO_INFO("Performance profile 'performance' set workaround applied\n");
