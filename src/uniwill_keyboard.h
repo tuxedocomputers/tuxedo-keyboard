67a80ca3a220c45cbc6780cee5d706ae555da774
Christoffer Sandberg <cs@tuxedo.de>
2022-09-23 11:45:40 +0200

    Remove XMG Fusion from three profiles list
    Not compatible with current control mechanism

--- src/uniwill_keyboard.h
+++ src/uniwill_keyboard.h
@@ -207,9 +207,11 @@ struct uniwill_device_features_t *uniwill_get_device_features(void)
 		|| dmi_match(DMI_BOARD_NAME, "POLARIS1701A2060")
 		|| dmi_match(DMI_BOARD_NAME, "POLARIS1701I1650TI")
 		|| dmi_match(DMI_BOARD_NAME, "POLARIS1701I2060")
-		|| dmi_match(DMI_BOARD_NAME, "LAPQC71A")
-		|| dmi_match(DMI_BOARD_NAME, "LAPQC71B")
-		|| dmi_match(DMI_PRODUCT_NAME, "A60 MUV")
+		// Note: XMG Fusion removed for now, seem to have
+		// neither same power profile control nor TDP set
+		//|| dmi_match(DMI_BOARD_NAME, "LAPQC71A")
+		//|| dmi_match(DMI_BOARD_NAME, "LAPQC71B")
+		//|| dmi_match(DMI_PRODUCT_NAME, "A60 MUV")
 	;
 
 	uw_feats->uniwill_profile_v1_three_profs_leds_only = false
