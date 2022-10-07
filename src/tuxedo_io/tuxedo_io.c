5ee0e18335738540c65f400fc62ab8efdc2b7b4f
Werner Sembach <wse@tuxedocomputers.com>
2022-10-07 10:52:56 +0200

    Fix conflicting ioctl numbers and update api version

--- src/tuxedo_io/tuxedo_io.c
+++ src/tuxedo_io/tuxedo_io.c
@@ -34,7 +34,7 @@
 
 MODULE_DESCRIPTION("Hardware interface for TUXEDO laptops");
 MODULE_AUTHOR("TUXEDO Computers GmbH <tux@tuxedocomputers.com>");
-MODULE_VERSION("0.2.5");
+MODULE_VERSION("0.2.6");
 MODULE_LICENSE("GPL");
 
 MODULE_ALIAS_CLEVO_INTERFACES();
